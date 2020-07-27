6d1195c6c0e4350e875e583d01076141975a8881
jihyoung-lee <wlgud3412@naver.com>
2020-07-27 22:49:14 +0900

    DB connect 수정

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -1,7 +1,18 @@
 <?php
-
-$mysqli = @mysqli_connect("localhost","root","0000","sign");
+//[1]
+/*$mysqli = @mysqli_connect("localhost","root","0000","sign");
 if (! $mysqli){
+    echo "연결에 실패하였습니다..";
+    exit();
+}
+*/
+$host = 'localhost';
+$user = 'root';
+$passWord = '0000';
+$dbName = 'sign';
+
+$mysqli = new mysqli($host,$user,$passWord,$dbName);
+if (! $mysqli) {
     echo "연결에 실패하였습니다..";
     exit();
 }
