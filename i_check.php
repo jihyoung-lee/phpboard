f1886a02addf8bcdee83c4dff6fbad58638ca7bf
jihyoung-lee <wlgud3412@naver.com>
2020-07-27 22:47:47 +0900

    Update i_check.php

--- i_check.php
+++ i_check.php
@@ -2,9 +2,9 @@
 ##세션 비교
 session_start();
 if (!isset($_SESSION['userid'])) {
-    echo "<a href=login.php>로그인</a>";
+    echo "<a href=login.php><input type='button' value='로그인'></a>";
     echo "<br>";
-    echo "<a href=signUp.html>회원가입</a>";
+    echo "<a href=signUp.html><input type='button' value='회원가입' ></a>";
 
     #로그인 전
 } elseif (isset($_SESSION['userid'])) {
@@ -12,9 +12,9 @@ if (!isset($_SESSION['userid'])) {
     echo $id;
     echo "님";
     echo "<br>";
-    echo "<a href=logout.php>로그아웃</a>";
+    echo "<a href=logout.php><input type='button'value='로그아웃'></a>";
     echo "<br>";
-    echo "<a href=userout.php>회원탈퇴</a>";
+   //구현 전 echo "<a href=userout.php>회원탈퇴</a>";
 }
 #로그인 후
 ?>
