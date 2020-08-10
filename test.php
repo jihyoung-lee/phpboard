44de6967d35c3234b2f1a3695aefbe54094e522a
jihyoung-lee <wlgud3412@naver.com>
2020-08-11 04:17:14 +0900

    Update test.php
    error

--- test.php
+++ test.php
@@ -1,11 +1,32 @@
 <?php
+
+
 include "mysqli_connect.php";
+include "session.php";
+/*
 $select_query = "SELECT * FROM list";
 $result_set = mysqli_query($mysqli,$select_query);
 while ($row = mysqli_fetch_row($result_set)){
-echo $row[0];
-    echo "<td style='width: 150px'>".$row[0]."</td>";
+echo $row[2];
+    echo "<td style='width: 150px'>".$row[2]."</td>";
 //print_r($row);
 echo '<br>';
 }
+
+*/
+$test = isset($_SESSION['userid']);
+$num = $_GET['num'];
+$select_query = "select * from list where list_no = $num";
+$result_set = mysqli_query($mysqli,$select_query);
+while ($row = mysqli_fetch_row($result_set)){
+    echo '$row 값 : '.$row[2];
+    echo '<br />';
+    echo '세션 값 : '.$_SESSION['userid'];
+    echo '<br />';
+    echo 'isset : '. $test;
+    echo '<br />';
+    echo '<br />';
+    print_r($row);
+    }
+
 ?>
