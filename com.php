b8070b7ea41790f0dc53f3fb08ad434d49b99082
jihyoung-lee <wlgud3412@naver.com>
2020-08-11 04:15:24 +0900

    comment

--- com.php
+++ com.php
@@ -6,9 +6,12 @@ $userid = $_SESSION['userid'];
 $com = $_POST['com'];
 $query = "INSERT INTO comment (list_no, userid, comment, date) VALUES ('$num','$userid','$com',now())";
 $write_up = mysqli_query($mysqli,$query);
+
 if($write_up) {
     echo "<script>alert('성공')</script>";
     echo "<script>history.back(-1)</script>";
+    $query2="update list set com_cnt=com_cnt+1 WHERE list_no=".$num;
+    $result_set2 = mysqli_query($mysqli,$query2);
 } elseif (! $write_up) {
     echo "실패 원인 :";
     echo mysqli_error($mysqli);
